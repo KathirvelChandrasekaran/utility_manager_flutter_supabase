0c3f46973309017e66ddb3671fc6f15b202ff389
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2021-10-10 07:36:14 +0530

    update: added delete and share task

--- lib/widgets/todo_body.dart
+++ lib/widgets/todo_body.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:lottie/lottie.dart';
+import 'package:share/share.dart';
 import 'package:utility_manager_flutter/providers/todo_list_provider.dart';
+import 'package:utility_manager_flutter/utils/constants.dart';
 
 Consumer todoBody() {
   return Consumer(builder: (context, watch, child) {
@@ -15,64 +18,98 @@ Consumer todoBody() {
               : ListView(
                   children: [
                     for (var i = 0; i < res.value.data.length; i++)
-                      Container(
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(15.0),
-                              child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Color(
-                                      int.parse(res.value.data[i]['color'])),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(20.0),
-                                  child: Column(
-                                    children: [
-                                      SizedBox(
-                                        height: 15,
-                                      ),
-                                      Text(
-                                        res.value.data[i]['task_name'],
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 25,
+                      Slidable(
+                        actionPane: SlidableDrawerActionPane(),
+                        actionExtentRatio: 0.25,
+                        secondaryActions: [
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: IconSlideAction(
+                              caption: 'Delete',
+                              color: Colors.blue,
+                              icon: Icons.delete_forever_rounded,
+                              onTap: () async {
+                                await supabase.from('TodoList').delete().match(
+                                    {'id': res.value.data[i]['id']}).execute();
+                                context.showSnackBar(
+                                    message: "Successfully deleted!");
+                                Navigator.pushReplacementNamed(
+                                    context, '/home');
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: IconSlideAction(
+                              caption: 'Share',
+                              color: Colors.indigo,
+                              icon: Icons.share,
+                              onTap: () {
+                                Share.share(
+                                    'Check out the todo ${res.value.data[i]['task_name']}');
+                              },
+                            ),
+                          ),
+                        ],
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color(
+                                        int.parse(res.value.data[i]['color'])),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: Column(
+                                      children: [
+                                        SizedBox(
+                                          height: 15,
                                         ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                      SizedBox(
-                                        height: 15,
-                                      ),
-                                      Text(
-                                        res.value.data[i]['task_description'],
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 20,
+                                        Text(
+                                          res.value.data[i]['task_name'],
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 25,
+                                          ),
+                                          textAlign: TextAlign.center,
                                         ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                      SizedBox(
-                                        height: 15,
-                                      ),
-                                      Text(
-                                        res.value.data[i]['date_time'],
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 25,
+                                        SizedBox(
+                                          height: 15,
                                         ),
-                                        textAlign: TextAlign.center,
-                                      )
-                                    ],
+                                        Text(
+                                          res.value.data[i]['task_description'],
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 20,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                        SizedBox(
+                                          height: 15,
+                                        ),
+                                        Text(
+                                          res.value.data[i]['date_time'],
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 25,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       )
                   ],
